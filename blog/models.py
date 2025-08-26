421eee0a0bf981c7d4e8312062d004dd34343e51
Klecianny Melo <klecianny.melo@quorum.us>
2025-08-26 16:31:00 -0300

    :sparkles: indexes and Status class

--- blog/models.py
+++ blog/models.py
@@ -3,18 +3,34 @@ from django.utils import timezone
 
 # Create your models here.
 class Post(models.Model):
+    class Status(models.Model):
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PB', 'Published'
+
+
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(
+        max_lenght=2,
+        choices=Status,
+        default=Status.DRAFT
+    )
 
-    # Order the posts by the inverse publish date
-    # The '-' sign means inverse
-    # The first post will be the most recent
+    
     class Meta:
+        # Order the posts by the inverse publish date
+        # The '-' sign means inverse
+        # The first post will be the most recent
         ordering = ['-publish']
+        # Create an index for the publish field
+        # This will improve the performance of the query
+        indexes = [
+            models.Index(fields=['-publish'])
+        ]
 
     # Return the title of the blog post as a string
     def __str__(self):
