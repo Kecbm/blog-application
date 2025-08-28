537120b085b45960b575bda680fc432666996148
Klecianny Melo <klecianny.melo@quorum.us>
2025-08-28 17:16:32 -0300

    :sparkles: author fiels in the Post model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
@@ -10,6 +11,13 @@ class Post(models.Model):
 
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
+    # Add many-to-one relationship
+    # One author can write many posts
+    author = models.foreignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='blog_posts'
+    )
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
