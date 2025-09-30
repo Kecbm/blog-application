08835c6c4f33d0ec8fee4cbf36a582369a86b638
Klecianny Melo <klecianny.melo@quorum.us>
2025-09-30 16:46:58 -0300

    :sparkles: add the model on the admin site

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+# Register the Post model with the admin site
+# admin.site.register(Post)
+
+# Define how field will be displayed in the admin site
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    # Customizing how models are displayed in the admin site
+    list_display = ['title', 'slug', 'author', 'publish', 'status']
+    list_filter = ['status', 'created', 'publish', 'author']
+    search_fields = ['title', 'body']
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ['author']
+    date_hierarchy = 'publish'
+    ordering = ['status', 'publish']
