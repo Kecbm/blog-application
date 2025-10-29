8084b8686fc9fe82690793e35b466428b06f6a37
Klecianny Melo <klecianny.melo@quorum.us>
2025-10-29 17:29:59 -0300

    :bug: adjusting to access a status in the views

--- blog/views.py
+++ blog/views.py
@@ -23,7 +23,7 @@ def post_detail(request, id):
     post = get_object_or_404(
         Post,
         id=id,
-        status=Post.status.PUBLISHED
+        status=Post.Status.PUBLISHED
     )
     
     return render(
