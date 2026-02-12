2f008cfa5fcb921e7e7bfcf259f7210733ac443c
Klecianny Melo <klecianny.melo@quorum.us>
2026-02-12 17:53:04 -0300

    :bug: path name of the post_list view

--- blog/urls.py
+++ blog/urls.py
@@ -5,13 +5,13 @@ app_name = 'blog'
 
 urlpatterns = [
     # post views
-    path('', views.post_list, name='post_list'),
-    
+    # path('', views.post_list, name='post_list'),
+
     # Old url for post:
     # path('<int:id>/', views.post_detail, name='post_detail'),
 
     # New url for post with class-based views:
-    # path('', views.PostListView.as_view(), name='post_list'),
+    path('', views.PostListView.as_view(), name='post_list'),
 
     # New url for post with SEO-friendly URLs:
     path(
