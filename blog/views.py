e6be2df6d4f8756cb1aec8efe8f65fd886b4820a
Klecianny Melo <klecianny.melo@quorum.us>
2026-02-25 18:05:46 -0300

    :sparkles: more configurations of searchVector

--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,11 @@ from django.views.generic import ListView
 from django.shortcuts import get_object_or_404, render
 # from django.http import Http404
 from django.views.decorators.http import require_POST
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import (
+    SearchVector,
+    SearchQuery,
+    SearchRank
+)
 from .forms import CommentForm, EmailPostForm, SearchForm
 from .models import Post
 from taggit.models import Tag
@@ -184,11 +188,17 @@ def post_search(request):
         
         if form.is_valid():
             query = form.cleaned_data['query']
+            search_vector = SearchVector('title', 'body')
+            search_query = SearchQuery(query)
             results = (
                 Post.published.annotate(
-                    search=SearchVector('title', 'body'),
+                    # search=SearchVector('title', 'body'),
+                    search=search_vector,
+                    rank=SearchRank(search_vector, search_query)
                 )
-                .filter(search=query)
+                # .filter(search=query)
+                .filter(search=search_query)
+                .order_by('-rank')
             )
 
     return render(
